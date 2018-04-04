72a5dee74e12f2d569d376a667407985304d8ebb
Fontana <matthieu.fontana@itcloud.ovh>
2018-04-04 22:57:35 +0200

    Initial commit

---
+++ audit_environment.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rep="resultats_`date "+%Y-%m-%d"`"
+mkdir "$rep"
+
+echo "Création de la liste des environnements à auditer"
+
+knife environment list | grep -E '(sig|phi|god|gre)_pr' >> "$rep/environment.list"
+
+echo "Création du fichier avec hostname des machines inclues dans les environnements"
+
+fichier1="$rep/environment.list"
+oldIFS=$IFS     # sauvegarde du séparateur de champ
+IFS=$'\n'       # nouveau séparateur de champ, le caractère fin de ligne
+
+for ligne1 in $(<$fichier1)
+do
+  knife node list -E $ligne1 >> "$rep/vms.list"
+done
+
+echo "Création du fichier avec configuration complète des VMs"
+
+fichier2="$rep/vms.list"
+oldIFS=$IFS
+IFS=$'\n'
+
+for ligne2 in $(<$fichier2)
+do
+  knife node show -l $ligne2 >> "$rep/audit_VMs.list"
+done
+
+echo "Création d un fichier audit simplifié"
+
+cat "$rep/audit_VMs.list" | grep -E '(Node Name:)|(Environment:)|([A-Z]::)|(kb_available:)|(^  name:)'  >> "$rep/audit_final_`date "+%Y-%m-%d"`.txt"
+
+# | sed ":a;N;s/\n\s\s\s//g" | sed "s/^\s\s//g"
